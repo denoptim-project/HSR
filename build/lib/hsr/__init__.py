34d734a19fa9fe22d2df621a667194124a92ed1c
Marcello Costamagna <marcello.costamagna@uib.no>
2025-03-25 09:15:16 +0100

    adjust pyproject to avoid 'post' releases

--- build/lib/hsr/__init__.py
+++
@@ -1,8 +0,0 @@
-from .fingerprint import *
-from .pca_transform import *
-from .pre_processing import *
-from .similarity import *
-from .utils import *
-from .hsr_cli import *
-from .version import *
-
